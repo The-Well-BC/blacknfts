e23ef90c32eb63d8132f44fb9de401f5b9c94676
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-27 14:24:20 +0100

    Add SuperRare, F8N, Zora artist files

--- nfteapp.com/js/randomNFT.js
+++ nfteapp.com/js/randomNFT.js
@@ -1,12 +1,50 @@
 let randomNFT = function() {
-    return fetch('../artists-rarible.csv'
-    ).then(res => {
-        return res.text();
-    }).then(csv => {
-        let artists = $.csv.toObjects(csv);
-        let tokenIDs = artists.map(a => a["TOKEN ID"]).filter(t => t != undefined && t != '');
+    let rarible, superrare, foundation, zora;
 
-        let randomIndex = Math.floor(Math.random() * tokenIDs.length);
-        return tokenIDs[randomIndex];
+    let promises = [
+        fetch('../artistCSVs/artists-rarible.csv'),
+        fetch('../artistCSVs/artists-superrare.csv'),
+        fetch('../artistCSVs/artists-foundation.csv'),
+        fetch('../artistCSVs/artists-zora.csv')
+    ];
+
+    return Promise.all(promises)
+    .then(res => {
+        let csvs = res.map(i => i.text());
+        return Promise.all(csvs);
+    }).then(csvs => {
+        let [rarible, superrare, foundation, zora] = csvs.map(i => $.csv.toObjects(i));
+        let tokens = [
+            rarible.map(a => { 
+                return { ...a, 
+                    contractAddr: '0x60f80121c31a0d46b5279700f9df786054aa5ee5'
+                }
+            }),
+            superrare.map(a => { 
+                return { ...a, 
+                    contractAddr: '0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0'
+                }
+            }),
+            foundation.map(a => { 
+                return { ...a, 
+                    contractAddr: '0x3B3ee1931Dc30C1957379FAc9aba94D1C48a5405'
+                }
+            }),
+            zora.map(a => { 
+                return { ...a, 
+                    contractAddr: '0xabEFBc9fD2F806065b4f3C237d4b59D9A97Bcac7'
+                }
+            }),
+        ].flat();
+
+        tokens = tokens.map(a => {
+            return {
+                contractAddr: a.contractAddr,
+                id: a["TOKEN ID"]
+            }
+        }).filter(t => t.id != undefined && t.id != '');
+
+        let randomIndex = Math.floor(Math.random() * tokens.length);
+        return tokens[randomIndex];
     });
 }
