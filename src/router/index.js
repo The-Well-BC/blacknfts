93666cb4ca74da713d25c405e9b562cd93cd1406
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-03-05 18:36:49 +0100

    Change routes to hash routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
+// import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/home.vue';
 import Artists from '../views/artistList.vue';
 
@@ -22,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
